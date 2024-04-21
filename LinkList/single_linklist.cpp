9074b461000a14e304af6bb6eafa75a8fcec4453
Vishal-Shandilya17334 <visushandilya@gmail.com>
2024-04-21 17:11:03 +0530

    linkList

---
+++ LinkList/single_linklist.cpp
@@ -0,0 +1,30 @@
+class node
+{
+    public:
+    int data;
+    class node *ptr= nullptr;
+    node* create_node()
+    {
+        node n1,*ptr;
+        ptr=&n1;
+        return ptr;
+
+    }
+    void push(int value)
+    {
+        node *ptr;
+        ptr=create_node();
+        ptr->date = value;
+        if
+    }
+};
+   node *start_node;
+
+   
+   
+
+   int main()
+   {
+       node n1;
+       n1.data=5;
+   }
